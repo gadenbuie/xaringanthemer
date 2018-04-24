db7f5dd977121cc761b49152365b52bd50d812d7
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-04-24 08:27:13 -0400

    Revert a25f7f32 - header text sizes in px
    Header fonts are generally not the same font as text font, so linking the two requires tweaking other properties (padding, etc.) of the header.

--- R/mono_light.R
+++ R/mono_light.R
@@ -26,9 +26,9 @@
 #' @param table_row_border_color Table row inner bottom border, defaults to #ddd
 #' @param table_row_even_background_color Table Even Row Background Color, defaults to `lighten_color(base_color, 0.8)`
 #' @param text_size Slide Body Text Size, defaults to 20px
-#' @param header_h1_font_size h1 Header Text Size, defaults to 2.75em
-#' @param header_h2_font_size h2 Header Text Size, defaults to 2.25em
-#' @param header_h3_font_size h3 Header Text Size, defaults to 1.75em
+#' @param header_h1_font_size h1 Header Text Size, defaults to 55px
+#' @param header_h2_font_size h2 Header Text Size, defaults to 45px
+#' @param header_h3_font_size h3 Header Text Size, defaults to 35px
 #' @param text_slide_number_font_size Slide Number Text Size, defaults to 0.9em
 #' @param code_inline_font_size Inline Code Text Size, defaults to 1em
 #' @param code_font_size Code Text Size, defaults to 0.9em
@@ -80,9 +80,9 @@ mono_light <- function(
   table_row_border_color = "#ddd",
   table_row_even_background_color = lighten_color(base_color, 0.8),
   text_size = "20px",
-  header_h1_font_size = "2.75em",
-  header_h2_font_size = "2.25em",
-  header_h3_font_size = "1.75em",
+  header_h1_font_size = "55px",
+  header_h2_font_size = "45px",
+  header_h3_font_size = "35px",
   text_slide_number_font_size = "0.9em",
   code_inline_font_size = "1em",
   code_font_size = "0.9em",
