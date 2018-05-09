5d5c894bab810370c496fd4ae00bad2287228064
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-05-09 12:55:57 -0400

    Add CSS Help vignette

--- make.R
+++ make.R
@@ -11,6 +11,10 @@ message("Rendering vignettes/template-variables.Rmd for GitHub")
 rmarkdown::render("template-variables.Rmd", output_format = rmarkdown::github_document(), quiet = TRUE)
 unlink("template-variables.html")
 
+message("Rendering vignettes/css-help.Rmd for GitHub")
+rmarkdown::render("css-help.Rmd", output_format = rmarkdown::github_document(), quiet = TRUE)
+unlink("css-help.html")
+
 setwd(here::here())
 message("Rendering README.Rmd for GitHub")
 rmarkdown::render("README.Rmd", quiet = TRUE)
