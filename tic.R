703765c40f50461b30bb322f8793b9a9acb85fbd
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-02-20 22:48:31 -0500

    travis: deploy pkgdown if env var exists

--- tic.R
+++ tic.R
@@ -1,7 +1,7 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 tic::do_package_checks()
 
-if (tic::ci_on_travis() && isTRUE(tic::ci_get_env("BUILD_PKGDOWN"))) {
+if (tic::ci_on_travis() && tic::ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   if (tic::ci_get_branch() == "dev") {
     tic::do_pkgdown(deploy = tic::ci_can_push())
