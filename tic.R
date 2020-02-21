080e6a2d0d9dc69da806310beb24c897090aa640
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-02-20 22:38:33 -0500

    deploy dev pkgdown site too

--- tic.R
+++ tic.R
@@ -1,7 +1,11 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks()
+tic::do_package_checks()
 
-if (ci_on_travis()) {
+if (tic::ci_on_travis() && isTRUE(tic::ci_get_env("BUILD_PKGDOWN"))) {
   # creates pkgdown site and pushes to gh-pages branch
-  do_pkgdown()
+  if (tic::ci_get_branch() == "dev") {
+    tic::do_pkgdown(deploy = tic::ci_can_push())
+  } else {
+    tic::do_pkgdown()
+  }
 }
