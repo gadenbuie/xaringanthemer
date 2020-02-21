059bea78997d0311130e1638a06244964005e11e
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-02-21 11:16:14 -0500

    [travis] log which branch, push pkgdown if dev

--- tic.R
+++ tic.R
@@ -3,8 +3,9 @@ tic::do_package_checks()
 
 if (tic::ci_on_travis() && tic::ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
+  message("Branch is: ", tic::ci_get_branch())
   if (tic::ci_get_branch() == "dev") {
-    tic::do_pkgdown(deploy = tic::ci_can_push())
+    tic::do_pkgdown(deploy = TRUE)
   } else {
     tic::do_pkgdown()
   }
