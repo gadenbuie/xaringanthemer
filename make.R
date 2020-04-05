08ef1715a0d337f2fd70b08f92cd21657dce02e9
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-04-05 07:37:20 -0400

    Add package check to make process

--- make.R
+++ make.R
@@ -4,10 +4,12 @@ callr::rscript("generate_theme_functions.R", wd = here::here("inst/scripts"))
 message("Writing documentation")
 devtools::document()
 
-setwd(here::here())
 message("Rendering README.Rmd for GitHub")
 rmarkdown::render("README.Rmd", quiet = TRUE)
 unlink("README.html")
+
+message("Checking package")
+devtools::check(document = FALSE)
 
 message("Building and Installing Package with Vignettes")
 devtools::install(build_vignettes = TRUE)
