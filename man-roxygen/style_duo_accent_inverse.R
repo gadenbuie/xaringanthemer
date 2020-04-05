b6df901b668c43f6a8021f4fdc6ec4a8af330661
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-04-05 07:56:48 -0400

    Add an example to each style function

--- man-roxygen/style_duo_accent_inverse.R
+++ man-roxygen/style_duo_accent_inverse.R
@@ -1,3 +1,16 @@
 #' @title Duotone Accent Inverse Theme
 #' @description An "inverted" default xaringan theme with a two colors used
 #'   for color accents on select elements (headers, bold text, etc.).
+#' @examples
+#' # Create a xaringan style in a temporary file
+#' xaringan_themer_css <- tempfile("xaringan-themer-", fileext = ".css")
+#'
+#' style_duo_accent_inverse(
+#'   primary_color = "#006747",
+#'   secondary_color = "#cfc493",
+#'   outfile = xaringan_themer_css # omit in your slides to write the
+#'                                 # styles to xaringan-themer.css
+#' )
+#'
+#' # View the CSS:
+#' # file.edit(xaringan_themer_css)
--- man-roxygen/style_mono_dark.R
+++ man-roxygen/style_mono_dark.R
@@ -1,2 +1,14 @@
 #' @title Monotone Dark Theme
 #' @description A dark monotone theme based around a single color.
+#' @examples
+#' # Create a xaringan style in a temporary file
+#' xaringan_themer_css <- tempfile("xaringan-themer-", fileext = ".css")
+#'
+#' style_mono_dark(
+#'   base_color = "#cbf7ed",
+#'   outfile = xaringan_themer_css # omit in your slides to write the
+#'                                 # styles to xaringan-themer.css
+#' )
+#'
+#' # View the CSS:
+#' # file.edit(xaringan_themer_css)
